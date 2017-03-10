90e0e44363de961582379bf357716cd5b6cff7e7
Vitaly Timoshchuk <vtimoshc@syr.edu>
2017-03-10 17:48:54 -0500

    I took care of some error handling

--- hw5.c
+++ hw5.c
@@ -5,11 +5,27 @@
 // Multiple header files
 #include "modifyChange.h" 
 #include "printInfoHelp.h"
+#include "Monitor.h"
 
 int main(int argc, char* argv[])
 {
+
+	 if (argc != 2)
+	{
+		return EXIT_FAILURE;
+	}
+    
+	const char* file = argv[1];
+        struct stat file_st;
+
+	if (stat(file, &file_st) < 0)
+	{
+        printf("-----NOT A FILE-----");
+        return EXIT_FAILURE;
+    	}
 	char* filename = argv[1];
 	printInfoHelp(filename);
 	
 	modifyChange(filename);
 }
+
