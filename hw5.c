b1f13630b50681e5db0cd538a467cb7282f069f0
Vitaly Timoshchuk <vtimoshc@syr.edu>
2017-03-10 20:18:19 -0500

    Modified hw5.c file -part3

--- hw5.c
+++ hw5.c
@@ -7,27 +7,35 @@
 #include "printInfoHelp.h"
 #include "Monitor.h"
 #include "Options.h"
+//#include "location.h"
+//#include "backupFile.h"
 
 int main(int argc, char* argv[])
 {
+	struct stat file_st;
+	char* file = argv[1];
 
-	 if (argc != 2)
+
+	if (argc != 2)
 	{
 		return EXIT_FAILURE;
 	}
-    
-	const char* file = argv[1];
-        struct stat file_st;
 
 	if (stat(file, &file_st) < 0)
 	{
         printf("-----NOT A FILE-----");
-        return EXIT_FAILURE;
-    	}
+        return 0;
+    }
+
+    if(argv[1] != -1)
+    {
+    	printf("Usage Information: %s\t\n", argv[0]);
+    }
+
 	char* filename = argv[1];
 	printInfoHelp(filename);
 	
 	modifyChange(filename);
-	Options(argc, argv[]);
+	Options(argc, argv);
 }
 
